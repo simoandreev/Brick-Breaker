a02e9012c923686a9a6d18f4a928fd3544f321d7
SimeonAndreev <simoandreev@gmail.com>
2015-12-10 17:09:58 +0200

    Initial Commit

---
+++ Brick Breaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Brick Breaker
+//
+//  Created by Simeon Andreev on 12/10/15.
+//  Copyright © 2015 Simeon Andreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Brick Breaker/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Brick Breaker
+//
+
+//  Copyright (c) 2015 Simeon Andreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
