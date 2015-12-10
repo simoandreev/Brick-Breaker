c7b800acc3f727c245aadb2c33bdf7574b8ab7d1
SimeonAndreev <simoandreev@gmail.com>
2015-12-10 18:19:18 +0200

    add paddle

--- Brick Breaker/GameScene.m
+++ Brick Breaker/GameScene.m
@@ -8,20 +8,26 @@
 
 #import "GameScene.h"
 
-@implementation GameScene
+@implementation GameScene {
+    SKSpriteNode *_paddle;
+    CGPoint _touchLocation;
+}
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
-    self.backgroundColor = [UIColor grayColor];
+    self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
     
+    //Setup Paddle
+    _paddle = [SKSpriteNode spriteNodeWithImageNamed:@"Paddle"];
+    _paddle.position = CGPointMake(self.size.width * 0.5, 90);
+    [self addChild:_paddle];
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
     for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
+        _touchLocation = [touch locationInNode:self];
     
     }
 }
@@ -30,4 +36,24 @@
     /* Called before each frame is rendered */
 }
 
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        // Calculate how far touch has moved on x axis.
+        CGFloat xMovement = [touch locationInNode:self].x - _touchLocation.x;
+        // Move paddle distance of touch.
+        _paddle.position = CGPointMake(_paddle.position.x + xMovement, _paddle.position.y);
+        
+        CGFloat paddleMinX = -_paddle.size.width * 0.25;
+        CGFloat paddleMaxX = self.size.width + (_paddle.size.width * 0.25);
+        // Cap paddle's position so it remains on screen.
+        if (_paddle.position.x < paddleMinX) {
+            _paddle.position = CGPointMake(paddleMinX, _paddle.position.y);
+        }
+        if (_paddle.position.x > paddleMaxX) {
+            _paddle.position = CGPointMake(paddleMaxX, _paddle.position.y);
+        }
+
+        _touchLocation = [touch locationInNode:self];
+    }
+}
 @end
--- Brick Breaker/GameViewController.m
+++ Brick Breaker/GameViewController.m
@@ -42,7 +42,7 @@
     skView.ignoresSiblingOrder = YES;
     
     // Create and configure the scene.
-    GameScene *scene = [GameScene unarchiveFromFile:@"GameScene"];
+    GameScene *scene = [GameScene sceneWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     // Present the scene.
